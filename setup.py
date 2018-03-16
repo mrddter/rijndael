f8c456b97631f3a90d044604c5ceae25e63caf4f
giopromolla <giopromolla@gmail.com>
2018-03-16 17:48:56 -0300

    upgrade version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='rijndael',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     test_suite='rijndael.tests',
     include_package_data=True,
